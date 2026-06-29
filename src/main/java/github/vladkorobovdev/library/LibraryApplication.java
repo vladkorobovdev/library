113813ca1f1ec19b9303f6d90d49cc13c4113f64
Uladzislau Korabau <vladkorobov.work@gmail.com>
2026-06-29 23:40:12 +0200

    init files

---
+++ src/main/java/github/vladkorobovdev/library/LibraryApplication.java
@@ -0,0 +1,13 @@
+package github.vladkorobovdev.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/github/vladkorobovdev/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package github.vladkorobovdev.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
